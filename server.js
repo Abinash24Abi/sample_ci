2e2defe22fb063e3102f05eb6f157559eada61e4
Abinash <127226250+Abinash24Abi@users.noreply.github.com>
2025-12-27 17:33:15 +0530

    update

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const express = require('express');
 const app = express();
 
 app.get("/" , (req,res) => {
-   res.send("Welcom to CI")
+   res.send("Welcom to CD")
 });
-
 
 
 const server = app.listen(8080,() => {
